ba26ae5c76f8e336a768d67437b53d2efc18ab4d
Philip Poppler <philpoppler@gmail.com>
2019-01-26 15:52:07 -0800

    Add JQuery FlipButton

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,6 +9,18 @@ class Home extends Component {
             <div>
                 <Navigation />
                 <Header />
+                <section id="faq">
+                    <div className="contents">
+                        <div className="faqelem">
+                            <div className="question">
+                                When is it?    
+                            </div>
+                            <div className="answer">
+                                BeachHacks is 24 hours long starting on April 13, 2019 and ending on April 14, 2019. A more detailed schedule will be released closer to the event.
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <Sponsors/>
             </div>
         );
