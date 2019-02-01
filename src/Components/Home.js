a3c75796c78af22063bffed27340c47de639f45c
Aleks <jokerka17@gmail.com>
2019-01-31 18:49:32 -0800

    Adding the info about BeachHacks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Navigation from './Navigation';
 import Sponsors from './Sponsors';
 import FAQ from './FAQ';
+import Index from './Index';
 
 export default class Home extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Home extends Component {
       <div>
         <Navigation />
         <Header />
+        <Index />
         <FAQ />
         <Sponsors />
       </div>
---
+++ src/Components/Index.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class Info extends Component {
+    render(){
+        return(
+            <div id="info">
+            <h1 classname="info-header">What is BeachHacks?</h1>
+                <p classname="info-container">
+                BeachHacks is the fourth collegiate hackathon hosted by California State University, Long Beach! 
+                During our 24 hour event, student developers and designers will gather on campus to build and program ideas into reality. 
+                Whether you join a team or go solo, join us at BeachHacks and show us what makes you unique. 
+                It doesn't matter if you're aiming to be the next Tony Stark or you're just looking to learn some code, 
+                BeachHacks has something for you!
+                </p>
+            </div>
+        )
+    }
+}
