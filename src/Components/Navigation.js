69900e2d377ab7150f9bdaa2b260d99d25601d6d
Philip Poppler <philpoppler@gmail.com>
2019-02-12 14:06:47 -0800

    Fix Registration

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,43 +1,52 @@
-import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-import Register from './Register';
-import Walter from './Walter';
+import React, { Component } from "react";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import Register from "./Register";
+import Walter from "./Walter";
 
 class Navigation extends Component {
-
-    render() {
-        return (
-            <div>
-                <Navbar collapseOnSelect fixedTop>
-                    <Navbar.Header>
-                        <Navbar.Brand >
-                            <AnchorLink href="#header" className="item-logo">
-                                <Walter />
-                            </AnchorLink>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav pullRight>
-                            <NavItem className="item" eventKey={0}>
-                                <AnchorLink offset='125' href="#info"><p>ABOUT</p></AnchorLink>
-                            </NavItem>
-                            <NavItem className="item" eventKey={1}>
-                                <AnchorLink offset='150' href="#faq"><p>FAQ</p></AnchorLink>
-                            </NavItem>
-                            <NavItem className="item" eventKey={2}>
-                                <AnchorLink href="#sponsors"><p>SPONSORS</p></AnchorLink>
-                            </NavItem>
-                            <NavItem className="item" eventKey={3}>
-                                <Register><a>REGISTER</a></Register>
-                            </NavItem>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect fixedTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <AnchorLink href="#header" className="item-logo">
+                <Walter />
+              </AnchorLink>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem className="item" eventKey={0}>
+                <AnchorLink offset="125" href="#info">
+                  <p>ABOUT</p>
+                </AnchorLink>
+              </NavItem>
+              <NavItem className="item" eventKey={1}>
+                <AnchorLink offset="150" href="#faq">
+                  <p>FAQ</p>
+                </AnchorLink>
+              </NavItem>
+              <NavItem className="item" eventKey={2}>
+                <AnchorLink offset="85"href="#sponsors">
+                  <p>SPONSORS</p>
+                </AnchorLink>
+              </NavItem>
+              <NavItem
+                className="item"
+                eventKey={3}
+                href="https://beachhacks.typeform.com/to/LGUC4U"
+              >
+                REGISTER
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
